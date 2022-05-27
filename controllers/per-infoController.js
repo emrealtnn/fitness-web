1381219e9e43165ad4f5e627a438173372ad497a
Emre ALTIN <emrealtn.268.ea@gmail.com>
2022-05-27 22:52:41 +0300

    commit

--- controllers/per-infoController.js
+++ controllers/per-infoController.js
@@ -2,12 +2,5 @@ const User= require("../models/User")
 const perinfoGet = (req, res) => {
     res.render('per-info');
 }
-const perinfoPost = (req,res) => {
-    const {boy,kilo}=req.body;  
-    try {
-    User.find()
-    } catch (error) {
-        console.log(error);
-    }  
-}
-module.exports = {perinfoGet,perinfoPost};
+
+module.exports = {perinfoGet};
