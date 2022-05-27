d409258ea3422d49013d69840ad4d2a4828ed57f
Emre ALTIN <emrealtn.268.ea@gmail.com>
2022-05-27 23:39:00 +0300

    commit

--- routes/per-infoRoute.js
+++ routes/per-infoRoute.js
@@ -4,5 +4,4 @@ const perinfoController = require('../controllers/per-infoController');
 const router = Router();
 
 router.get('/per-info',perinfoController.perinfoGet);
-router.post('/per-info', perinfoController.perinfoPost);
 module.exports = router;
